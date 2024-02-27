34c9f0fff4a6f074b312e5d961ed12a1cf1edb3d
Bulazs <mcbulazslol@gmail.com>
2024-02-27 12:36:37 +0100

    alt line moving

--- lua/bulazs/remap.lua
+++ lua/bulazs/remap.lua
@@ -7,3 +7,16 @@ vim.api.nvim_create_autocmd('filetype', {
     end
 })
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.api.nvim_set_keymap('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
