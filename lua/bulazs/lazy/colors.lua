9d90be3117c6c2086641780f3c1526670f580128
Bulazs <mcbulazslol@gmail.com>
2024-02-29 00:42:44 +0100

    created transparent nvim

--- lua/bulazs/lazy/colors.lua
+++ lua/bulazs/lazy/colors.lua
@@ -1,12 +1,40 @@
-return {
+local isTransparent = true
 
-         'navarasu/onedark.nvim',
-   
-    config = function()
-        require('onedark').setup {
-            style = 'darker'
-        }
-        require('onedark').load()
-        vim.cmd('colorscheme onedark')
-    end
+function ToggleTransparent()
+    isTransparent = not isTransparent
+    vim.cmd('Lazy reload ' .. vim.g.colors_name)
+end
+
+return {
+    {
+        'navarasu/onedark.nvim',
+        name = 'onedark',
+        config = function()
+            require('onedark').setup {
+                transparent = isTransparent,
+                style = 'deep',
+            }
+            require('onedark').load()
+            vim.cmd('colorscheme onedark')
+        end
+    },
+    {
+        "folke/tokyonight.nvim",
+        name = "tokyonight",
+        config = function()
+            require("tokyonight").setup({
+                style = "storm",
+                transparent = isTransparent,
+                terminal_colors = true,
+                dim_inactive = false,
+                styles = {
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                    -- Background styles. Can be "dark", "transparent" or "normal"
+                    --sidebar = "transparent",
+                    --float = "transparent",
+                },
+            })
+        end
+    },
 }
