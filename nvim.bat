425e8aba50985264f665cb4a6abffc5d87f9d285
Bulazs <mcbulazslol@gmail.com>
2024-02-28 23:29:58 +0100

    hotfix

--- nvim.bat
+++ nvim.bat
@@ -1,14 +1,15 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
 if "%~1"=="" (
     set "unixPath=~"
 ) else (
     cd /d %~1
-    set "currentDirectory=%cd%"
-    set "driveLetter=%currentDirectory:~0,1%"
-    set "pathWithoutDrive=%currentDirectory:~3%"
-    set "unixPath=/mnt/%driveLetter%/%pathWithoutDrive%"
+    if errorlevel 1 (
+        set "unixPath=~"
+    ) else (
+        set "workingDir=!cd!"
+        set "unixPath=/mnt/!workingDir:~0,1!/!workingDir:~3!"
+    )
 )
-
-wt.exe new-tab -p Ubuntu -- wsl.exe -d Ubuntu /usr/bin/zsh -ci "cd $(echo '%unixPath%' | tr '[:upper:]' '[:lower:]' | sed 's:\\:/:g') && nvim -c 'Ex'&& zsh -i"
+wt.exe new-tab -p Ubuntu -- wsl.exe -d Ubuntu /usr/bin/zsh -ci "cd $(echo '!unixPath!' | tr '[:upper:]' '[:lower:]' | sed 's:\\:/:g') && nvim -c 'Ex'&& zsh -i"
