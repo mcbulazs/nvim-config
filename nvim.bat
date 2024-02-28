16ad96e0d0f0170d3531bc016a149f26296ed438
Bulazs <mcbulazslol@gmail.com>
2024-02-28 22:18:56 +0100

    fixed bat file so that it can open without a path

--- nvim.bat
+++ nvim.bat
@@ -1,8 +1,14 @@
 @echo off
 setlocal
-cd /d %~1
-set "currentDirectory=%cd%"
-set "driveLetter=%currentDirectory:~0,1%"
-set "pathWithoutDrive=%currentDirectory:~3%"
 
-wt.exe new-tab -p Ubuntu -- wsl.exe -d Ubuntu /usr/bin/zsh -ci "cd $(echo /mnt/'%driveLetter%/%pathWithoutDrive%' | tr '[:upper:]' '[:lower:]' | sed 's:\\:/:g') && nvim -c 'Ex'&& zsh -i"
+if "%~1"=="" (
+    set "unixPath=~"
+) else (
+    cd /d %~1
+    set "currentDirectory=%cd%"
+    set "driveLetter=%currentDirectory:~0,1%"
+    set "pathWithoutDrive=%currentDirectory:~3%"
+    set "unixPath=/mnt/%driveLetter%/%pathWithoutDrive%"
+)
+
+wt.exe new-tab -p Ubuntu -- wsl.exe -d Ubuntu /usr/bin/zsh -ci "cd $(echo '%unixPath%' | tr '[:upper:]' '[:lower:]' | sed 's:\\:/:g') && nvim -c 'Ex'&& zsh -i"
