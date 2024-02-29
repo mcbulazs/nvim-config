40ab5ed9d40f007fcbd8be435c3b8f7147829c14
Bulazs <mcbulazslol@gmail.com>
2024-02-29 22:51:50 +0100

    fixed an issue regarding "

--- nvim.bat
+++ nvim.bat
@@ -9,7 +9,10 @@ if "%~1"=="" (
         set "unixPath=~"
     ) else (
         set "workingDir=!cd!"
+        set "workingDir=!workingDir:\=/!"
         set "unixPath=/mnt/!workingDir:~0,1!/!workingDir:~3!"
+        
     )
 )
 wt.exe new-tab -p Ubuntu -- wsl.exe -d Ubuntu /usr/bin/zsh -ci "cd $(echo '!unixPath!' | tr '[:upper:]' '[:lower:]' | sed 's:\\:/:g') && nvim -c 'Ex'&& zsh -i"
+endlocal
