ae8aeb206fd88c6847694c3d53ca05509540a352
Bulazs <mcbulazslol@gmail.com>
2024-02-29 22:36:08 +0100

    set up references with telescope

--- lua/bulazs/lazy/lsp.lua
+++ lua/bulazs/lazy/lsp.lua
@@ -102,8 +102,6 @@ return {
                 vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
                 vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
                 vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-
-                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
                 vim.keymap.set("n", "<leader>f", function()
                     vim.lsp.buf.format({ async = true })
                     vim.cmd("write")
--- lua/bulazs/lazy/telescope.lua
+++ lua/bulazs/lazy/telescope.lua
@@ -1,17 +1,40 @@
+local function openReferences()
+    require("telescope.builtin").lsp_references({
+        layout_strategy = "horizontal",
+        layout_config = {
+            width = 0.6,
+            height = 0.5,
+            prompt_position = "bottom",
+        },
+        sorting_strategy = "ascending",
+        ignore_filename = false,
+        show_line = false,
+        trim_text = true,
+        include_declaration = false,
+        include_current_line = false,
+    })
+end
+
 return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
 
     dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
     },
     config = function()
-        require('telescope').setup({})
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>g', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<C-f>', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") });
+        require("telescope").setup({
+            defaults = {
+                path_display = { "truncate" },
+            },
+        })
+
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>g", builtin.find_files, {})
+        vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+        vim.keymap.set("n", "gr", openReferences, { noremap = true, silent = true })
+        vim.keymap.set("n", "<C-f>", function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
-    end
+    end,
 }
