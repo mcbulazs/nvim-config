881fcdac1805f6aa599853ea21c67f900e9e599d
Bulazs <mcbulazslol@gmail.com>
2024-03-05 10:45:09 +0100

    setup for prettier

--- lua/bulazs/lazy/nonels.lua
+++ lua/bulazs/lazy/nonels.lua
@@ -1,7 +1,7 @@
 return {
-    'nvimtools/none-ls.nvim',
+    "nvimtools/none-ls.nvim",
     dependencies = {
-        'nvim-lua/plenary.nvim'
+        "nvim-lua/plenary.nvim",
     },
     config = function()
         local null_ls = require("null-ls")
@@ -12,14 +12,15 @@ return {
 
                 null_ls.builtins.formatting.gofumpt,
                 null_ls.builtins.formatting.goimports_reviser,
-                null_ls.builtins.formatting.golines,
                 null_ls.builtins.diagnostics.golangci_lint,
 
-                null_ls.builtins.formatting.biome,
-
+                null_ls.builtins.formatting.prettierd.with({
+                    env = {
+                        PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.config/nvim/utils/linter-config/.prettierrc.json"),
+                    },
+                }),
                 null_ls.builtins.formatting.clang_format,
-
             },
         })
-    end
+    end,
 }
