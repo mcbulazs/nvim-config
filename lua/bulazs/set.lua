7bb04b5625ea162404ab15c79fff345ae56469c6
Bulazs <mcbulazslol@gmail.com>
2024-05-06 17:53:55 +0200

    biome added to nonels

--- lua/bulazs/set.lua
+++ lua/bulazs/set.lua
@@ -4,6 +4,9 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.cmd([[
+  autocmd FileType c,cpp,hpp,h setlocal shiftwidth=2
+]])
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
