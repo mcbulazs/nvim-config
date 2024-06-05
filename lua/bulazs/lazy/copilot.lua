0480669cb6eabc19d3e2d3c66e42bc28548b43d8
Bulazs <mcbulazslol@gmail.com>
2024-06-05 23:21:03 +0200

    new copilot

--- lua/bulazs/lazy/copilot.lua
+++ lua/bulazs/lazy/copilot.lua
@@ -1,3 +1,25 @@
 return {
-        'github/copilot.vim',
+    "zbirenbaum/copilot.lua",
+    config = function()
+        require("copilot").setup({
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                keymap = {
+                    accept = "<S-Tab>",
+                    accept_word = "<C-l>",
+                    accept_line = "<C-A-l>",
+                    next = "<C-j>",
+                    prev = "<C-k>",
+                    dismiss = "<Esc>",
+                },
+            },
+            panel = { enabled = false },
+        })
+        local suggestion = require("copilot.suggestion")
+        vim.keymap.set("i", "<C-Right>", suggestion.accept_word)
+        vim.keymap.set("i", "<CA-Right>", suggestion.accept_line)
+        vim.keymap.set("i", "<C-Down>", suggestion.next)
+        vim.keymap.set("i", "<C-Up>", suggestion.prev)
+    end,
 }
