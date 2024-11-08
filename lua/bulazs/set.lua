2c4f08fd7f1759c95a55cf429d712ccb0c678889
Vari Balazs <vari.balazs01@gmail.com>
2024-11-08 17:04:40 +0100

    set base tab identation to 2 sapces

--- lua/bulazs/set.lua
+++ lua/bulazs/set.lua
@@ -1,10 +1,10 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
+vim.opt.tabstop = 2
 
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.cmd([[
   autocmd FileType c,cpp,hpp,h setlocal shiftwidth=2
 ]])
