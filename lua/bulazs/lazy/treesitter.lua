13cc38c7f9db164a0a633389bb9e633888406a6b
Vari Balazs <vari.balazs01@gmail.com>
2025-01-23 19:15:26 +0100

    cleaning up treesitter

--- lua/bulazs/lazy/treesitter.lua
+++ lua/bulazs/lazy/treesitter.lua
@@ -1,10 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-context",
+  },
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      --ensure_installed = { "c", "lua", "vim", "cpp", "go" },
-      --sync_install = false,
       auto_install = true,
       indent = {
         enable = true,
