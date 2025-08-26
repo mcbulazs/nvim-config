21565872cb123cc6596333de5eb8aac68f7951d7
Vari Balazs <vari.balazs01@gmail.com>
2025-08-26 23:09:36 +0200

    lualine now shows oil dir

--- lua/bulazs/lazy/lualine.lua
+++ lua/bulazs/lazy/lualine.lua
@@ -1,8 +1,16 @@
 return {
   "AndreM222/copilot-lualine",
-  dependencies = { "nvim-tree/nvim-web-devicons", "nvim-lualine/lualine.nvim", "folke/trouble.nvim" },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "nvim-lualine/lualine.nvim",
+    "folke/trouble.nvim",
+    "stevearc/oil.nvim",
+  },
   config = function()
     local lualine = require("lualine")
+    local function getOilPath()
+      return require("oil").get_current_dir()
+    end
 
     lualine.setup({
       options = {
@@ -11,7 +19,20 @@ return {
       sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { "filename" },
+        lualine_c = {
+          {
+            getOilPath,
+            cond = function()
+              return require("oil").get_current_dir() ~= nil
+            end,
+          },
+          {
+            "filename",
+            cond = function()
+              return require("oil").get_current_dir() == nil
+            end,
+          },
+        },
         lualine_x = { "copilot", "encoding", "fileformat", "filetype" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
